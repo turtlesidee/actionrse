f12cf7f7fe1afbf90045dcc4c03ef97cf6d29a6d
LEROY alexandre <leroy.alexandre@turtleside.io>
2022-09-16 20:23:33 +0200

    feat/main add auth0

--- apps/app/pages/_app.tsx
+++ apps/app/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import './styles.css';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -9,7 +10,9 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to app!</title>
       </Head>
       <main className="app">
-        <Component {...pageProps} />
+        <UserProvider>
+          <Component {...pageProps} />
+        </UserProvider>
       </main>
     </>
   );
